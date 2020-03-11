7ee338508cc07c6b10747a9e0872fa8d00d989af
Matuk <mateusmatuk@fai-mg.br>
2020-03-11 19:43:09 -0300

    primeiro commit

---
+++ src/br/fai/collection/Main.java
@@ -0,0 +1,17 @@
+package br.fai.collection;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		System.out.println("oi eu sou o goku");
+	}
+
+}
